9b62e6330fc8f868a9652b4c7774edac71aab833
feel0729 <feel0729@gmail.com>
2022-12-14 09:50:37 +0800

    Wei

--- analyze table.sql
+++ analyze table.sql
@@ -1,3 +1,7 @@
+-- Oracle sql
+-- Analze tables to update statistics , 
+-- that CBO(cost base optimizer) governs the execution for every Oracle query.
+-- HOW TO USE: excute this script and excute result of this script.
 select 'ANALZE TABLE "' || ut.table_name || '" COMPUTE STATISTICS;'
 from user_tables ut
 ;
